612af10a27e7c3c7e0b0ae3712974c5d2e8491f3
Max Bebök <beboek.max@gmail.com>
2018-03-29 20:57:45 +0200

    index for imports

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = 
+{
+    Creator: require("src/binary_file_creator.js"),
+    Parser : require("src/binary_file_parser.js"),
+    File   : require("src/binary_file.js"),
+    Loader : require("src/file_loader.js")
+};
